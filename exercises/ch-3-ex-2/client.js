f23e7d3fa4b92f5d4dca837c53b4080d76435489
reginaldoJuniior <junior.corvette2@gmail.com>
2025-07-31 13:04:25 +0100

    Adding refresh token implementation

--- exercises/ch-3-ex-2/client.js
+++ exercises/ch-3-ex-2/client.js
@@ -133,22 +133,57 @@ app.get('/fetch_resource', function(req, res) {
 		res.render('data', {resource: body});
 		return;
 	} else {
-		/*
-		 * Instead of always returning an error like we do here, refresh the access token if we have a refresh token
-		 */
-		console.log("resource status error code " + resource.statusCode);
-		res.render('error', {error: 'Unable to fetch resource. Status ' + resource.statusCode});
+		access_token = null;
+		if (refresh_token) {
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', {error: resource.statusCode})
+			return;
+		}
 	}
 	
 	
 });
 
 var refreshAccessToken = function(req, res) {
+	var form_data = qs.stringify({
+		grant_type: 'refresh_token',
+		refresh_token: refresh_token
+	});
 
-	/*
-	 * Use the refresh token to get a new access token
-	 */
-	
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint,
+		{
+			body: form_data,
+			headers: headers
+		}
+	);
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		var body = JSON.parse(tokRes.getBody());
+
+		access_token = body.access_token;
+		console.log('Got new access token: %s', access_token);
+
+		if (body.refresh_token) {
+			refresh_token = body.refresh_token;
+			console.log('Got new refresh token: %s', refresh_token);
+		}
+
+		scope = body.scope;
+		console.log('Got scope: %s', scope);
+
+		res.redirect('/fetch_resource')
+	} else {
+		console.log('Unable to refresh access token, server response: %s', tokRes.statusCode);
+		refresh_token = null;
+		res.render('error', {error: 'Unable to refresh access token, server response: ' + tokRes.statusCode});
+	}
 };
 
 var buildUrl = function(base, options, hash) {

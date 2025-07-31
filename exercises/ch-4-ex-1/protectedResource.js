32852529892304ac189bb4463adf615aeeb6f0a4
reginaldoJuniior <junior.corvette2@gmail.com>
2025-07-31 19:44:14 +0100

    implementing oauth security to the resource server

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -23,23 +23,47 @@ var resource = {
 };
 
 var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
-	
+	var inToken = null;
+    var auth = req.headers['authorization'];
+
+    // Check the Authorization header, body, or query string for the access token
+    if (auth && auth.toLowerCase().indexOf('bearer') === 0) {
+        inToken = auth.slice('bearer '.length);
+    } else if (req.body && req.body.access_token) {
+        inToken = req.body.access_token;
+    } else if (req.query && req.query.access_token) {
+        inToken = req.query.access_token;
+    }
+
+    console.log('Incoming token: %s', inToken);
+    nosql.one().make(function(builder) {
+        builder.where('access_token', inToken);
+        builder.callback(function(err, token) {
+            if (token) {
+                console.log("We found a matching token: %s", inToken);
+            } else {
+                console.log('No matching token was found.');
+            }
+            req.access_token = token;
+            next();
+        });
+    });
 };
 
-app.options('/resource', cors());
+// Add the getAccessToken function to all requests
+app.all('*', getAccessToken);
 
+app.options('/resource', cors());
 
 /*
  * Add the getAccessToken function to this handler
  */
 app.post("/resource", cors(), function(req, res){
-
-	/*
-	 * Check to see if the access token was found or not
-	 */
+    if (req.access_token) {
+        res.json(resource)
+    } else {
+        res.status(401).json({error: 'Unauthorized', error_description: 'Access token is missing or invalid.'});
+    }
 	
 });
 

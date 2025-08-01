f53c3071c4945b8bf536474f9785f592f6255bce
reginaldoJuniior <junior.corvette2@gmail.com>
2025-08-01 11:00:37 +0100

    Adding lowcarb to the exercise

--- exercises/ch-4-ex-3/protectedResource.js
+++ exercises/ch-4-ex-3/protectedResource.js
@@ -58,13 +58,23 @@ var requireAccessToken = function(req, res, next) {
 };
 
 app.get('/produce', getAccessToken, requireAccessToken, function(req, res) {
-	var produce = {fruit: ['apple', 'banana', 'kiwi'], 
-		veggies: ['lettuce', 'onion', 'potato'], 
-		meats: ['bacon', 'steak', 'chicken breast']};	
+	var produce = {fruit: [], veggies: [],	meats: [], lowcarb: []};
 
-	/*
-	 * Add different kinds of produce based on the incoming token's scope
-	 */
+	if (__.contains(req.access_token.scope, 'meats')) {
+		produce.meats = ['bacon', 'steak', 'chicken breast'];
+	}
+
+	if (__.contains(req.access_token.scope, 'fruit')) {
+		produce.fruit =  ['apple', 'banana', 'kiwi'];
+	}
+
+	if (__.contains(req.access_token.scope, 'veggies')) {
+		produce.veggies = ['lettuce', 'onion', 'potato'];
+	}
+
+	if (__.contains(req.access_token.scope, 'lowcarb')) {
+		produce.lowcarb = ['broccoli', 'cauliflower', 'zucchini'];
+	}
 
 	res.json(produce);
 });

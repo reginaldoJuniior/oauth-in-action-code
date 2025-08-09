29c41aef62e3ee9af22c6ceeb2f5e8aeb6f49142
reginaldoJuniior <junior.corvette2@gmail.com>
2025-08-09 16:53:14 +0100

    chapter 8 ex 2 adding CORS support to the implicit grant type

--- exercises/ch-8-ex-2/protectedResource.js
+++ exercises/ch-8-ex-2/protectedResource.js
@@ -84,7 +84,10 @@ var requireAccessToken = function(req, res, next) {
 	}
 }; 
 
-app.get("/helloWorld", getAccessToken, function(req, res){
+var cors = require('cors');
+
+app.options('/helloWorld', cors());
+app.get("/helloWorld", cors(), getAccessToken, function(req, res){
 	if (req.access_token) {
 		
 		res.setHeader('X-Content-Type-Options','nosniff');
@@ -94,15 +97,15 @@ app.get("/helloWorld", getAccessToken, function(req, res){
 			"greeting" : ""
 		};
 
-		if (req.query.language == "en") {
+		if (req.query.language === "en") {
 			resource.greeting = 'Hello World';
-		} else if (req.query.language == "de") {
+		} else if (req.query.language === "de") {
 			resource.greeting ='Hallo Welt';
-		} else if (req.query.language == "it") {
+		} else if (req.query.language === "it") {
 			resource.greeting = 'Ciao Mondo';
-		} else if (req.query.language == "fr") {
+		} else if (req.query.language === "fr") {
 			resource.greeting = 'Bonjour monde';
-		} else if (req.query.language == "es") {
+		} else if (req.query.language === "es") {
 			resource.greeting ='Hola mundo';
 		} else {
 			resource.greeting = "Error, invalid language: "+ querystring.escape(req.query.language);

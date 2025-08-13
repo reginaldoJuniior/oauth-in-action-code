5a03f794365bbcaa02f91d720b181b64679974bb
reginaldoJuniior <junior.corvette2@gmail.com>
2025-08-13 16:55:49 +0100

    chapter 11 ex 2 including token JWT signature to the authorization server and protected resource

--- exercises/ch-11-ex-2/protectedResource.js
+++ exercises/ch-11-ex-2/protectedResource.js
@@ -55,31 +55,33 @@ var getAccessToken = function(req, res, next) {
 	var payload = JSON.parse(base64url.decode(tokenParts[1]));
 	console.log('Payload', payload);
 
-	/*
-	 * Validate the signature of the JWT
-	 */
+	// Validate the signature of the JWT
+	if (jose.jws.JWS.verify(inToken,
+		Buffer.from(sharedTokenSecret).toString('hex'),
+		[header.alg])) {
 
-	if (payload.iss == 'http://localhost:9001/') {
-		console.log('issuer OK');
-		if ((Array.isArray(payload.aud) && __.contains(payload.aud, 'http://localhost:9002/')) || 
-			payload.aud == 'http://localhost:9002/') {
-			console.log('Audience OK');
-			
-			var now = Math.floor(Date.now() / 1000);
-			
-			if (payload.iat <= now) {
-				console.log('issued-at OK');
-				if (payload.exp >= now) {
-					console.log('expiration OK');
-					
-					console.log('Token valid!');
-	
-					req.access_token = payload;
-					
+		console.log('Signature OK');
+
+		if (payload.iss === 'http://localhost:9001/') {
+			console.log('issuer OK');
+			if ((Array.isArray(payload.aud) && __.contains(payload.aud, 'http://localhost:9002/')) ||
+				payload.aud === 'http://localhost:9002/') {
+				console.log('Audience OK');
+
+				var now = Math.floor(Date.now() / 1000);
+
+				if (payload.iat <= now) {
+					console.log('issued-at OK');
+					if (payload.exp >= now) {
+						console.log('expiration OK');
+
+						console.log('Token valid!');
+
+						req.access_token = payload;
+					}
 				}
 			}
 		}
-		
 	}
 			
 	next();
